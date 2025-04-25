bd7df61295b93196f73aaa53d11fbaa9d209a84a
Artem <postaluka07@gmail.com>
2025-04-25 14:17:48 +0300

    no control

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -14,7 +14,7 @@ export default class Camera
         this.canvas = this.experience.canvas
 
         this.setInstance()
-        this.setControl()
+        // this.setControl()
 
         this.debug()
 
@@ -104,7 +104,7 @@ export default class Camera
 
     update()
     {
-        this.controls.update()
+        // this.controls.update()
         // console.log(
         //     this.instance.position,
         //     this.instance.rotation
